c58725e1704a993517a93787ba479ffc94517b52
akash's app <chourasiaakash05@gmail.com>
2018-06-17 05:36:01 +0530

    create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,3 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: {minimum: 3,maximum: 50}
-    validates :description, presence: true,length: {minimum: 5,maximum: 500}
+   
 end
