f2fbb66a0de9e689d55a095e94975b3ee4f84fc6
akash's app <chourasiaakash05@gmail.com>
2018-06-18 02:59:44 +0530

    List Articles, created partials and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles=Article.all
+  end
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -11,9 +18,27 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was Updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
   def show
     @article = Article.find(params[:id])
   end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was Deleted"
+      redirect_to article_path
+
+  end
+  
   private
     def article_params
         params.require(:article).permit(:title ,:description)
