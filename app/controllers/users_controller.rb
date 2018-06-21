389273c09af3552b7c43de9625edd9ea71700e62
akash's app <chourasiaakash05@gmail.com>
2018-06-22 03:43:58 +0530

    Sign Up users from UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController 
+    def new
+        @user = User.new
+    end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success]="Welcome To the Alpha Blog #{@user.username}"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+    private
+    def user_params
+        params.require(:user).permit(:username,:email,:password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :articles
   root 'welcome#home'
   get 'about', to: 'welcome#about'
-  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
