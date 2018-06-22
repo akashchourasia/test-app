6429c2579838a3c0366c48448d8139166a2bd567
akash's app <chourasiaakash05@gmail.com>
2018-06-23 02:52:38 +0530

    fixing some problems for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180622193330) do
+ActiveRecord::Schema.define(version: 20180622212001) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 20180622193330) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "password_digest"
   end
 
 end
