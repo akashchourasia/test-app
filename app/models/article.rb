59f01417f0e92cd9f077aa00071461717cba425b
akashchourasia <chourasiaakash05@gmail.com>
2018-06-24 12:00:41 +0530

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,6 @@ class Article < ApplicationRecord
 
 
    validates :title, presence: true, length: {minimum: 3, maximum: 50}
-   validates :description, presence: true, length: {minimum: 10, maximum: 500}
+   validates :description, presence: true, length: {minimum: 10}
    validates :user_id, presence: true
 end
