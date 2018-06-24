73dfc9716c8bee83bd7b1755cd2fd1da546d0f13
akash's app <chourasiaakash05@gmail.com>
2018-06-24 06:11:04 +0530

    Added User admin requirements and navbar edited

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,35 +1,59 @@
 class CategoriesController < ApplicationController
+
+    before_action :require_admin, except: [:index, :show]
+    
     def index
-        @categories = Category.all
+    
+    @categories = Category.all
+    
     end
     
     def new
-        @category = Category.new 
+    
+    @category = Category.new
+    
     end
+    
     def create
-        @category = Category.new(category_params)
-
-        if @category.save
-        
-            flash[:success] = "Category was created successfully"
-            redirect_to categories_path
-        
-        else
-        
-            render 'new'
-        
-        end
+    
+    @category = Category.new(category_params)
+    
+    if @category.save
+    
+    flash[:success] = "Category was created successfully"
+    
+    redirect_to categories_path
+    
+    else
+    
+    render 'new'
+    
+    end
+    
     end
     
     def show
     
     end
+    
     private
-
+    
     def category_params
     
     params.require(:category).permit(:name)
     
     end
     
-end
+    def require_admin
+    
+    if !logged_in? || (logged_in? and !current_user.admin?)
+    
+    flash[:danger] = "Only admins can perform that action"
+    
+    redirect_to categories_path
+    
+    end
+    
+    end
+    
+    end
