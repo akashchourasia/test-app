894d14e6afbe99d192011e28feebc8d4f22f3253
akashchourasia <chourasiaakash05@gmail.com>
2018-06-24 19:15:02 +0530

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
     has_many :categories, through: :article_categories
 
 
-   validates :title, presence: true, length: {minimum: 3, maximum: 50}
+   validates :title, presence: true, length: {minimum: 3}
    validates :description, presence: true, length: {minimum: 10}
    validates :user_id, presence: true
 end
