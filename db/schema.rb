9900ef47ac588c143c3967155a976b50b1c2d9af
akash's app <chourasiaakash05@gmail.com>
2018-06-24 06:30:43 +0530

    Added Articles Categories Assiciation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180622233128) do
+ActiveRecord::Schema.define(version: 20180624004313) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
